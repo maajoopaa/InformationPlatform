b4fc9d8f0db42c9bddaff6cf9ff028f889a5ef92
maajoopaa <dhdhsjgsh@gmail.com>
2025-11-09 00:36:38 +0300

    Added login request dto

---
+++ InformationPlatform.Application/Models/Requests/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace InformationPlatform.Application.Models.Requests;
+
+public class LoginRequest
+{
+    public string Username { get; set; } = null!;
+    
+    public string Password { get; set; } = null!;
+}
