517d5d210810fff6f1fff4b4c444c5159d510660
maajoopaa <dhdhsjgsh@gmail.com>
2025-11-08 17:56:17 +0300

    Implemented base repository template

---
+++ Templates/Repositories/BaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Templates.Repositories.Interfaces;
+
+namespace Templates.Repositories;
+
+public class BaseRepository<TEntity, TContext> : IBaseRepository<TEntity> 
+    where TEntity : BaseDbEntityWithId
+    where TContext : DbContext
+{
+    private readonly TContext _context;
+    private readonly DbSet<TEntity> _dbSet;
+
+    public BaseRepository(TContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public virtual async Task AddAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        await _dbSet.AddAsync(entity, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public virtual async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        _dbSet.Update(entity);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public virtual async Task DeleteAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        _dbSet.Remove(entity);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public virtual async Task<List<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken)
+    {
+        return await _dbSet.Where(predicate).ToListAsync(cancellationToken);
+    }
+
+    public virtual async Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _dbSet.FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
+    }
+}
---
+++ Templates/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Templates.Repositories.Interfaces;
+
+public interface IBaseRepository<TEntity> where TEntity : BaseDbEntityWithId
+{
+    Task AddAsync(TEntity entity, CancellationToken cancellationToken);
+    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken);
+    Task DeleteAsync(TEntity entity, CancellationToken cancellationToken);
+    Task<List<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
+    Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+}
