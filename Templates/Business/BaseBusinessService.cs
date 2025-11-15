be5cea5802c07dc2c8f8f34a9245a3a1bc660492
maajoopaa <dhdhsjgsh@gmail.com>
2025-11-15 19:04:24 +0300

    Added base business service template

---
+++ Templates/Business/BaseBusinessService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Templates.Business;
+
+public class BaseBusinessService : IBaseBusinessService
+{
+    public BaseBusinessService(IHttpContextAccessor httpContextAccessor)
+    {
+        UserId = GetCurrentUserId(httpContextAccessor);
+    }
+
+    public Guid UserId { get; set; }
+
+    public Guid GetCurrentUserId(IHttpContextAccessor httpContextAccessor)
+    {
+        var claim = httpContextAccessor.HttpContext?.User?.FindFirst("id")?.Value;
+        return Guid.TryParse(claim, out var id) ? id : Guid.Empty;
+    }
+}
---
+++ Templates/Business/Interfaces/IBaseBusinessService.cs
@@ -0,0 +1,6 @@
+﻿namespace Templates.Business;
+
+public interface IBaseBusinessService
+{
+    Guid UserId { get; set; }
+}
