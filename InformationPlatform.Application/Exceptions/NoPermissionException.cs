646a65c78859df82746dc58ae4bedfb0d9e166a4
maajoopaa <dhdhsjgsh@gmail.com>
2025-11-15 19:26:47 +0300

    Added no permission custom exception

---
+++ InformationPlatform.Application/Exceptions/NoPermissionException.cs
@@ -0,0 +1,6 @@
+﻿namespace InformationPlatform.Application.Exceptions;
+
+public class NoPermissionException(string message) : Exception(message)
+{
+    public int StatusCode { get; set; } = 403;
+}
